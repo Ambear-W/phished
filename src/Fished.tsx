f8059b28682ea4401f9f3117dd22110b3beac6fb
Ambear-W <wells.amber16@gmail.com>
2025-06-01 12:39:32 -0400

    setting up before mutli page

--- src/Fished.tsx
+++ src/Fished.tsx
@@ -4,9 +4,18 @@ function Fished() {
   // want to render an h1 element =
   return (
     <>
-    <div className="fish-container adaptive">
-      <h1 className="fish-text adaptive">You've Been Phished</h1>
-      <img className="fish-icon adaptive" src={fish} alt="Fish" />
+    <div className="center">
+      <div className="fish-container adaptive">
+        <h1 className="fish-text adaptive">You've Been Phished</h1>
+        <img className="fish-icon adaptive" src={fish} alt="Fish" />
+      </div>
+      <p>
+        Uh oh! You clicked a link you were supposed to! Lucky for you that this isn't the real deal! But if it was, what would have happened? Well, you might not notice it right away,
+        but you would have given us your log in details!
+        <br />
+        <br />
+        Read more below:
+      </p>
     </div>
     </>
   );
