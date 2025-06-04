933868185a9a33e787980d0634878118dc542df1
Ambear-W <wells.amber16@gmail.com>
2025-06-03 21:38:09 -0400

    pushing again

--- src/Fished.tsx
+++ src/Fished.tsx
@@ -1,5 +1,7 @@
 import fish from './assets/fish.svg';
 import sso from './assets/sso.png';
+import nrgrok from './assets/phish_other_terminal.jpg';
+import setool from './assets/phish_terminal_w_info.jpg';
 
 function Fished() {
   // want to render an h1 element =
@@ -27,11 +29,26 @@ function Fished() {
           At Georgia Tech, Single Sign-on is used so users do not have to repeatedly log into campus applications. As adversaries, we can leverage this by designing our phishing flow to mimic the SSO experience.
           This makes it appear as though users need to login pior to scheduling their meeting. Since this is not atypical, they are less likely to question the legitimacy of the SSO page.
         </p>
-        <img className="sso" src={ sso } alt="Fish" />
+        <img className="sso" src={ sso } alt="Georgia Tech's SSO login page" />
         <p>
           While the users "signs in", on the backend we are stealing their username and password. This can be done with the SEToolkit that uses a tunner to open the localhost connection to recevie the credientials by using
           nrgrok, which is a port fowarding tool.
         </p>
+        <div className='sideRight'>
+          <p>
+            Here, we can see the nrgrok tool running.
+          </p>
+          <img className='sideImg' src={ nrgrok } alt="Nrgrok running on a terminal" />
+        </div>
+        <br />
+        <hr />
+        <br />
+        <div className='sideRight'>
+          <p>
+            Here, we can see the SEToolKit running. This is where we can also see the tested username and password.
+          </p>
+          <img className='sideImg' src={ setool } alt="SEToolKit running, the username and password have been highlighted" />
+        </div>
       </div>
     </div>
     </>
