04e8ac2a12ccbe1101570165c2ce23f1cbc753b1
Ambear-W <wells.amber16@gmail.com>
2025-06-03 21:17:51 -0400

    wanting to see how it looks

--- src/Fished.tsx
+++ src/Fished.tsx
@@ -1,4 +1,5 @@
 import fish from './assets/fish.svg';
+import sso from './assets/sso.png';
 
 function Fished() {
   // want to render an h1 element =
@@ -9,13 +10,29 @@ function Fished() {
         <h1 className="fish-text adaptive">You've Been Phished</h1>
         <img className="fish-icon adaptive" src={fish} alt="Fish" />
       </div>
-      <p>
-        Uh oh! You clicked a link you were not supposed to! Lucky for you that this isn't the real deal! But if it was, what would have happened? Well, you might not notice it right away,
-        but you would have given us your log in details!
-        <br />
-        <br />
-        Read more below:
-      </p>
+      <div>
+        <p>
+          Uh oh! You clicked a link you were not supposed to! Lucky for you that this isn't the real deal! But if it was, what would have happened? Well, you might not notice it right away,
+          but you would have given us your log in details!
+          <br />
+          <br />
+          So what would have happened?
+        </p>
+      </div>
+      <br />
+      <hr />
+      <br />
+      <div>
+        <p>
+          At Georgia Tech, Single Sign-on is used so users do not have to repeatedly log into campus applications. As adversaries, we can leverage this by designing our phishing flow to mimic the SSO experience.
+          This makes it appear as though users need to login pior to scheduling their meeting. Since this is not atypical, they are less likely to question the legitimacy of the SSO page.
+        </p>
+        <img className="sso" src={ sso } alt="Fish" />
+        <p>
+          While the users "signs in", on the backend we are stealing their username and password. This can be done with the SEToolkit that uses a tunner to open the localhost connection to recevie the credientials by using
+          nrgrok, which is a port fowarding tool.
+        </p>
+      </div>
     </div>
     </>
   );
